9725876c3bfe855ed452a5fd3da88529849173b7
Khaled Mohamed <khaled.mohamed@moselaymd.com>
2026-07-14 22:20:56 +0300

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   // Static Site Generator — fully prerendered output.
+  site: "https://KhaledMohamed08.github.io",
+  base: "/landing-test",
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
